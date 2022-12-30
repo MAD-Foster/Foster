db0777dac9282a365092d0c786c57a5e328ec326
Zaem <92740260+ZaemAzmi@users.noreply.github.com>
2022-12-30 12:51:45 +0800

    added some nav files

--- app/src/main/java/com/example/foster/LoginActivity.java
+++ app/src/main/java/com/example/foster/LoginActivity.java
@@ -7,18 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_login);
 
         Button BtnSubmit=findViewById(R.id.BtnSubmit);
         BtnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i =new Intent(new Intent(HomeActivity.this,MainActivity.class));
+                Intent i =new Intent(new Intent(LoginActivity.this,MainActivity.class));
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/foster/MainActivity.java
+++ app/src/main/java/com/example/foster/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.foster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,4 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    private void setupBottomNavMenu(NavController navController) {
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_view);
+        NavigationUI.setupWithNavController(bottomNav, navController, false);
+    }
+
+    private void setupNavMenu(NavController navController) {
+        NavigationView sideNav = findViewById(R.id.sideNav);
+        NavigationUI.setupWithNavController(sideNav, navController, false);
+    }
 }

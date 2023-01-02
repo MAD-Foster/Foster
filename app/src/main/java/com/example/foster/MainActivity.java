2bf920afbafd98745d6e1b40c3c36dde3ce258bb
Zaem <92740260+ZaemAzmi@users.noreply.github.com>
2023-01-02 19:18:41 +0800

    removed firebase related dependencies/file, improved side nav

--- app/src/main/java/com/example/foster/MainActivity.java
+++ app/src/main/java/com/example/foster/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.foster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,35 +24,31 @@ import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
-
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        Toolbar toolbar = findViewById(R.id.TBMainAct);
+        setSupportActionBar(toolbar);
+        navigationView = findViewById(R.id.sideNav);
+        navigationView.setItemIconTintList(null);
 
 
-//        NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.NHFMain);
-//        assert host != null;
-//        NavController navController = host.getNavController();
+        drawerLayout = findViewById(R.id.DLMain);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//
-//        setupBottomNavMenu(navController);
-
-//        Toolbar toolbar = findViewById(R.id.TBMainAct);
-//        setSupportActionBar(toolbar);
-//        CoordinatorLayout coordinatorLayout = findViewById(R.id.CLMain);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, coordinatorLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        coordinatorLayout.addDrawerListener(toggle);
-//        toggle.syncState();
-//        setupNavMenu(navController);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
@@ -54,9 +56,58 @@ public class MainActivity extends AppCompatActivity {
 //        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_view);
 //        NavigationUI.setupWithNavController(bottomNav, navController, false);
 //    }
-////
+//
+//    //
 //    private void setupNavMenu(NavController navController) {
 //        NavigationView sideNav = findViewById(R.id.sideNav);
 //        NavigationUI.setupWithNavController(sideNav, navController, false);
 //    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.DestHome) {
+
+        } else if (item.getItemId() == R.id.DestAboutApp) {
+
+        } else if (item.getItemId() == R.id.DestLogout) {
+        } else if (item.getItemId() == R.id.DestMusicPage) {
+        } else if (item.getItemId() == R.id.DestSettings) {
+        } else if (item.getItemId() == R.id.DestWorkoutPage) {
+            Intent i = new Intent(MainActivity.this, WorkoutActivity.class);
+            startActivity(i);
+        } else if (item.getItemId() == R.id.DestStepsPage) {
+
+        }
+
+        drawerLayout = findViewById(R.id.DLMain);
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_overflow, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/example/foster/WorkoutActivity.java
+++ app/src/main/java/com/example/foster/WorkoutActivity.java
@@ -20,22 +20,24 @@ public class WorkoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout);
 
-        binding = ActivityWorkoutBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        Toolbar toolbar = binding.toolbar;
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
-
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        binding = ActivityWorkoutBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        Toolbar toolbar = binding.toolbar;
+//        setSupportActionBar(toolbar);
+//        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+//        toolBarLayout.setTitle(getTitle());
+//
+//        FloatingActionButton fab = binding.fab;
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }

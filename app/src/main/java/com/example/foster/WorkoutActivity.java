9ddc9ea0b4fb05ee0bde5c3c0d5c38c361316109
Zaem <92740260+ZaemAzmi@users.noreply.github.com>
2023-01-03 19:55:04 +0800

    scrolling workoutpage

--- app/src/main/java/com/example/foster/WorkoutActivity.java
+++ app/src/main/java/com/example/foster/WorkoutActivity.java
@@ -1,43 +1,97 @@
 package com.example.foster;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.foster.databinding.ActivityWorkoutBinding;
 
-public class WorkoutActivity extends AppCompatActivity {
+public class WorkoutActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private ActivityWorkoutBinding binding;
+    //    private ActivityWorkoutBinding binding;
+    BottomNavigationView bottomNavigationView;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
+        Toolbar toolbar = findViewById(R.id.TBWorkoutAct);
+        setSupportActionBar(toolbar);
+        navigationView = findViewById(R.id.sideNav);
+        navigationView.setItemIconTintList(null);
 
-//        binding = ActivityWorkoutBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        Toolbar toolbar = binding.toolbar;
-//        setSupportActionBar(toolbar);
-//        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-//        toolBarLayout.setTitle(getTitle());
-//
-//        FloatingActionButton fab = binding.fab;
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
+        drawerLayout = findViewById(R.id.DLWorkout);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_overflow, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.DestHome) {
+
+        } else if (item.getItemId() == R.id.DestAboutApp) {
+
+        } else if (item.getItemId() == R.id.DestLogout) {
+        } else if (item.getItemId() == R.id.DestMusicPage) {
+        } else if (item.getItemId() == R.id.DestSettings) {
+        } else if (item.getItemId() == R.id.DestWorkoutPage) {
+            Intent i = new Intent(WorkoutActivity.this, WorkoutActivity.class);
+            startActivity(i);
+        } else if (item.getItemId() == R.id.DestStepsPage) {
+
+        }
+
+        drawerLayout = findViewById(R.id.DLMain);
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }

682384d0909cf9eab1ea63c41920d4ff09149482
Zaem <92740260+ZaemAzmi@users.noreply.github.com>
2023-01-08 15:04:04 +0800

    change image text

--- app/src/main/java/com/example/foster/StartingWorkoutFragment.java
+++ app/src/main/java/com/example/foster/StartingWorkoutFragment.java
@@ -1,5 +1,6 @@
 package com.example.foster;
 
+import android.annotation.SuppressLint;
 import android.media.Image;
 import android.os.Bundle;
 
@@ -15,6 +16,7 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +24,7 @@ import java.util.HashMap;
  * create an instance of this fragment.
  */
 public class StartingWorkoutFragment extends Fragment {
-
+    String value;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -56,7 +58,10 @@ public class StartingWorkoutFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        SpecificExerciseActivity specificExerciseActivity = (SpecificExerciseActivity) getActivity();
+        value = specificExerciseActivity.getCondition();
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -68,15 +73,14 @@ public class StartingWorkoutFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
+//        setExerciseTextAndImage();
         return inflater.inflate(R.layout.fragment_starting_workout, container, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//        ExerciseObject exerciseObject=new ExerciseObject();
-//        exerciseObject.textView=view.findViewById(R.id.TVFirstExercise);
-//        exerciseObject.imageView=view.findViewById(R.id.IVFirstExercise);
+//        View view = inflater.inflate(R.layout.fragment_starting_workout, container, false);
         firstTV = view.findViewById(R.id.TVFirstExercise);
         secondTV = view.findViewById(R.id.TVSecondExercise);
         thirdTV = view.findViewById(R.id.TVThirdExercise);
@@ -87,11 +91,9 @@ public class StartingWorkoutFragment extends Fragment {
         thirdIV = view.findViewById(R.id.IVThirdExercise);
         fourthIV = view.findViewById(R.id.IVFourthExercise);
         fifthIV = view.findViewById(R.id.IVFifthExercise);
-
-        firstTV.setText("");
+        setExerciseTextAndImage();
 
     }
-
 
     private TextView firstTV,
             secondTV,
@@ -105,15 +107,60 @@ public class StartingWorkoutFragment extends Fragment {
             fifthIV;
 //    HashMap<String,> hashMap = new HashMap<>();
 
-    TextView[] tvArr = {firstTV,secondTV,thirdTV,fourthTV,fifthTV};
-    ImageView[] ivArr = {firstIV,secondIV,thirdIV,fourthIV,fifthIV};
 
-//    public void setExerciseTextAndImage() {
+    public void setExerciseTextAndImage() {
+        TextView[] tvArr = {firstTV, secondTV, thirdTV, fourthTV, fifthTV};
+        ImageView[] ivArr = {firstIV, secondIV, thirdIV, fourthIV, fifthIV};
+        //    BEGINNER_DAY_1("Jumping jacks", " Push-ups", "Lunges", " Plank", "Bicycle crunches"),
+        int[] beginnerDay1 = {R.drawable.img_jumpingjack,R.drawable.img_pushup,R.drawable.img_lunges,R.drawable.img_plank,R.drawable.img_bcrucnhes};
+        Object[] array;
+//        Bundle b = this.getArguments();
+//        String value = b.getString("key");
+//        switch (value) {
+//            case "beginner_day1":
+//                array = ExerciseEnum.BEGINNER_DAY_1.getExercises().values().toArray();
+//                break;
+//            case "beginner_day2":
+//                array = ExerciseEnum.BEGINNER_DAY_2.getExercises().values().toArray();
+//                break;
+//            case "beginner_day3":
+//                array = ExerciseEnum.BEGINNER_DAY_3.getExercises().values().toArray();
+//                break;
+//            case "beginner_day4":
+//                array = ExerciseEnum.BEGINNER_DAY_4.getExercises().values().toArray();
+//                break;
+//            case "beginner_day5":
+//                array = ExerciseEnum.BEGINNER_DAY_5.getExercises().values().toArray();
+//                break;
+//            case "beginner_day6":
+//                array = ExerciseEnum.BEGINNER_DAY_6.getExercises().values().toArray();
+//                break;
+//            case "beginner_day7":
+//                array = ExerciseEnum.BEGINNER_DAY_7.getExercises().values().toArray();
+//                break;
+//            default:
+//                array = ExerciseEnum.BEGINNER_DAY_1.getExercises().values().toArray();
 //
-//        ExerciseObject exerciseObject = new ExerciseObject();
-//        exerciseObject.textView = findViewById()
-//        hashMap.put("first", )
-//    }
+//        }
+        Map<String, int[]> exerciseImage = new HashMap<>();
+
+//        exerciseImage.put("beginner_day1", )
+        Map<String, Map<String, String>> exerciseText = new HashMap<>();
+        exerciseText.put("beginner_day1", ExerciseEnum.BEGINNER_DAY_1.getExercises());
+        exerciseText.put("beginner_day2", ExerciseEnum.BEGINNER_DAY_2.getExercises());
+        exerciseText.put("beginner_day3", ExerciseEnum.BEGINNER_DAY_3.getExercises());
+        exerciseText.put("beginner_day4", ExerciseEnum.BEGINNER_DAY_4.getExercises());
+        exerciseText.put("beginner_day5", ExerciseEnum.BEGINNER_DAY_5.getExercises());
+        exerciseText.put("beginner_day6", ExerciseEnum.BEGINNER_DAY_6.getExercises());
+        exerciseText.put("beginner_day7", ExerciseEnum.BEGINNER_DAY_7.getExercises());
+        Map<String, String> chosenExerciseMap = exerciseText.get(value);
+        // Then, to retrieve the values for a given key:
+        array = chosenExerciseMap.values().toArray();
+        for (int i = 0; i < chosenExerciseMap.size(); i++) {
+            tvArr[i].setText((String) array[i]);
+            ivArr[i].setImageResource(beginnerDay1[i]);
+        }
+    }
 }
 
 class ExerciseObject {

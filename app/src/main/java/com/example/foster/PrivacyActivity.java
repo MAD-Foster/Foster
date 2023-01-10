1d6b386e4e692f6a351234f11e6090614da3509b
ijjue <izzureensaffiah@gmail.com>
2023-01-11 02:29:33 +0800

    :(

---
+++ app/src/main/java/com/example/foster/PrivacyActivity.java
@@ -0,0 +1,16 @@
+package com.example.foster;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrivacyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_privacy);
+
+
+    }
+}
